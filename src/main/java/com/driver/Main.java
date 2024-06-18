2dc0f85d25d734dd8d2e279cef5563899ee1fc1d
vaibhavshinde7887 <vshinde7887@gmail.com>
2024-06-19 02:28:45 +0530

    encapsulation assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+     RWOnly rwOnly = new RWOnly();
+//     RWOnly.name = "Vaibhav";
+//     System.out.println(RWOnly.name);
+        rwOnly.setName("Vaibhav");
+        System.out.println(rwOnly.setName());
+    }
 }
